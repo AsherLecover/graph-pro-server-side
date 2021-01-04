db0fbf5b2096ce510f6afb4d91a4b847087d81c0
AsherLecover <asherlec5@gmail.com>
2021-01-04 16:24:12 +0200

    initail commit

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -2,30 +2,30 @@ import { IsString, Matches, MaxLength, MinLength, IsEmail } from "class-validato
 
 export class AuthCredentialsDto {
 
-    @MinLength(2)
-    @MaxLength(20)
-    @IsString()
+    // @MinLength(2)
+    // @MaxLength(20)
+    // @IsString()
     username: string;
 
-    @IsEmail()
+    // @IsEmail()
     email: string;
 
-    @MinLength(4)
-    @MaxLength(20)
-    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {message: 'password pattern is too week'} )
-    @IsString()
+    // @MinLength(4)
+    // @MaxLength(20)
+    // @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {message: 'password pattern is too week'} )
+    // @IsString()
     password: string;
 }
 
 export class AuthCredentialsDtoSignin {
 
-    @IsEmail()
+    // @IsEmail()
     email: string;
   
-    @IsString()
-    @MinLength(6)
-    @MaxLength(20)
-    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, { message: 'password must have at least one uppercase letter, a lowercase a number, and a special character' })
+    // @IsString()
+    // @MinLength(6)
+    // @MaxLength(20)
+    // @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, { message: 'password must have at least one uppercase letter, a lowercase a number, and a special character' })
     password: string;
   
    
